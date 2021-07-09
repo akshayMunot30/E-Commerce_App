6a0db6a25e3e26901b43a6dad8a6624c0158924e
akshaymunot30 <akshaymunot30@gmail.com>
2021-07-09 20:12:11 +0530

    create account page: set buffer,enbale email from firebase, focus, next & done field and password(*) field

--- lib/screens/registerPage.dart
+++ lib/screens/registerPage.dart
@@ -5,12 +5,57 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class RegisterPage extends StatefulWidget {
-  
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  // Build an alert dialog to display some errors
+  Future<void> _alertDialogBuilder() async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Error"),
+          content: Container(
+            child: Text("Just some random text for now"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Close Dialogue"),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  // Default Form loading state
+  bool _registerFormLoading = false;
+
+  // Form Input Field values
+  String _registerEmail = "";
+  String _registerPassword = "";
+
+  // Focus Node for input fields
+  FocusNode? _passwordFocusNode;
+
+  @override
+  void initState() {
+    _passwordFocusNode = FocusNode();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _passwordFocusNode!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,13 +77,35 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
               Column(
                 children: [
-                  CustomInput(hintText: "Email ID",),
-                  CustomInput(hintText: "Password",),
+                  CustomInput(
+                    hintText: "Email ID",
+                    onChanged: (value) {
+                      _registerEmail = value;
+                    },
+                    onSubmitted: (value) {
+                      _passwordFocusNode!.requestFocus();
+                    },
+                    textInputAction: TextInputAction.next,
+                    isPasswordField: false,
+                  ),
+                  CustomInput(
+                    hintText: "Password",
+                    onChanged: (value) {
+                      _registerPassword = value;
+                    },
+                    textInputAction: TextInputAction.done,
+                    focusNode: _passwordFocusNode,
+                    isPasswordField: true,
+                  ),
                   CustomButton(
                     text: "Create New Account",
                     onPressed: () {
-                      print("Click the Login Button");
+                      //open dialog
+                      setState(() {
+                        _registerFormLoading = true;
+                      });
                     },
+                    isLoading: _registerFormLoading,
                     outlineBtn: false,
                   ),
                 ],
--- lib/widgets/custom_input.dart
+++ lib/widgets/custom_input.dart
@@ -2,12 +2,24 @@ import 'package:e_commerce_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class CustomInput extends StatelessWidget {
-  final String hintText;
+  final String? hintText;
+  final void Function(String)? onChanged;
+  final void Function(String)? onSubmitted;
+  final FocusNode? focusNode;
+  final TextInputAction? textInputAction;
+  final bool? isPasswordField;
 
-  CustomInput({required this.hintText});
+  CustomInput(
+      {this.hintText,
+      this.onChanged,
+      this.onSubmitted,
+      this.focusNode,
+      this.textInputAction,
+      this.isPasswordField});
 
   @override
   Widget build(BuildContext context) {
+    bool _isPasswordField = isPasswordField ?? false;
     return Container(
       margin: EdgeInsets.symmetric(
         horizontal: 24.0,
@@ -18,6 +30,11 @@ class CustomInput extends StatelessWidget {
         borderRadius: BorderRadius.circular(12.0),
       ),
       child: TextField(
+        obscureText: _isPasswordField,
+        focusNode: focusNode,
+        onChanged: onChanged,
+        onSubmitted: onSubmitted,
+        textInputAction: textInputAction,
         decoration: InputDecoration(
           border: InputBorder.none,
           hintText: hintText,
