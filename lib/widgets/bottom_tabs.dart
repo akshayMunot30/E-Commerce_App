aa0bf4d97dc815fc8c7e406da19594318499a32d
akshaymunot30 <akshaymunot30@gmail.com>
2021-08-20 23:06:15 +0530

    .

--- lib/widgets/bottom_tabs.dart
+++ lib/widgets/bottom_tabs.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 class BottomTabs extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
-            child: BottomTabButton(),
-          );
+      child: BottomTabButton(),
+    );
   }
 }
 
 class BottomTabButton extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Image.asset("assets\images\tab_home.jpeg")
+      child: Image(
+        image: AssetImage('assets\images\logout.png'),
+      ),
     );
   }
 }
