bbf8bfa46b60af8aec9eb5332b4a376d334f25a7
akshaymunot30 <akshaymunot30@gmail.com>
2021-08-21 18:00:40 +0530

    Added basic bottom tabs with indicators

--- lib/widgets/bottom_tabs.dart
+++ lib/widgets/bottom_tabs.dart
@@ -4,17 +4,66 @@ class BottomTabs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: BottomTabButton(),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12.0), topRight: Radius.circular(12.0)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            spreadRadius: 1.0,
+            blurRadius: 30.0,
+          )
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          BottomTabButton(
+            imagePath: 'assets/images/tab_home.png',
+            selected: true,
+          ),
+          BottomTabButton(
+            imagePath: 'assets/images/tab_search.png',
+            selected: false,
+          ),
+          BottomTabButton(
+            imagePath: 'assets/images/tab_bookmark.png',
+            selected: false,
+          ),
+          BottomTabButton(
+            imagePath: 'assets/images/tab_logout.png',
+            selected: false,
+          ),
+        ],
+      ),
     );
   }
 }
 
 class BottomTabButton extends StatelessWidget {
+  final String imagePath;
+  final bool selected;
+  BottomTabButton({required this.imagePath, required this.selected});
+
   @override
   Widget build(BuildContext context) {
+    bool _selected = selected;
+
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+      decoration: BoxDecoration(
+          border: Border(
+              top: BorderSide(
+        color: Theme.of(context).accentColor,
+        width: 2.0,
+      ))),
       child: Image(
-        image: AssetImage('assets/images/tab_home.png'),
+        image: AssetImage(
+          imagePath,
+        ),
+        width: 21.0,
+        height: 21.0,
       ),
     );
   }
